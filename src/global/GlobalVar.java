9da263edb6216a9226394b0def500cfabe74e7c8
jiang <1297551359@qq.com>
2019-05-18 16:06:53 +0800

    添加全局变量类，生成数据

--- src/global/GlobalVar.java
+++ src/global/GlobalVar.java
@@ -11,35 +11,43 @@ import java.util.List;
  * 全部变量，演员，道具，场景
  * */
 public class GlobalVar {
-    private List<Actor_> actor_list;
-    private List<Scene_> scene_list;
-    private List<Tool_> tool_list;
+    private static List<Actor_> actor_list;
+    private static List<Scene_> scene_list;
+    private static List<Tool_> tool_list;
 
+    static {
+        //生成演员数据
+        for(int i =0;i< 10;i++){
+            Actor_ actor_ = new Actor_();
+            actor_.setId(i);
+            actor_list.add(actor_);
+        }
+        //生成场景数据
+        for(int i =0;i< 5;i++){
+            Scene_ scene_ = new Scene_();
+            scene_.setId(i);
+            scene_list.add(scene_);
+        }
+        //生成道具数据
+        for(int i =0;i< 10;i++){
+            Tool_ tool_ = new Tool_();
+            tool_.setId(i);
+            tool_list.add(tool_);
+        }
+    }
     public GlobalVar() {
 
     }
 
-    public List<Actor_> getActor_list() {
+    public static List<Actor_> getActor_list() {
         return actor_list;
     }
 
-    public void setActor_list(List<Actor_> actor_list) {
-        this.actor_list = actor_list;
-    }
-
-    public List<Scene_> getScene_list() {
+    public static List<Scene_> getScene_list() {
         return scene_list;
     }
 
-    public void setScene_list(List<Scene_> scene_list) {
-        this.scene_list = scene_list;
-    }
-
-    public List<Tool_> getTool_list() {
+    public static List<Tool_> getTool_list() {
         return tool_list;
-    }
-
-    public void setTool_list(List<Tool_> tool_list) {
-        this.tool_list = tool_list;
     }
 }
