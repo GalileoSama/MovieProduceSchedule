ce8ae6cb83d2de13addac9f4c33fa0abe5664a31
王俊杰 <galileo233@163.com>
2019-05-18 15:28:18 +0800

    创建类

---
+++ dataGenerator/src/entity/Actor.java
@@ -0,0 +1,9 @@
+package entity;
+
+/**
+ * @author galileo
+ * @date 2019/5/18 15:26
+ */
+public class Actor {
+    private int id;
+}
---
+++ dataGenerator/src/entity/Scene.java
@@ -0,0 +1,9 @@
+package entity;
+
+/**
+ * @author galileo
+ * @date 2019/5/18 15:27
+ */
+public class Scene {
+    private int id;
+}
